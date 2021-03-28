d76d9d8cd8dd8c7ebc2fe302503da3e5d9322786
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-28 16:26:21 +0900

    Chapter8.4 ACM

--- src/main.tf
+++ src/main.tf
@@ -254,18 +254,46 @@ resource "aws_lb_listener" "http" {
     }
 }
 
-resource "aws_route53_record" "example" {
-    zone_id = data.aws_route53_zone.example.zone_id
-    name = data.aws_route53_zone.example.name
-    type = "A"
+# data "aws_route53_zone" "example" {
+#     name = "example.com"
+#     zone_id = "example.com"
+# }
 
-    alias {
-        name = aws_lb.example.dns_name
-        zone_id = aws_lb.example.zone_id
-        evaluate_target_health = true
-    }
-}
+# resource "aws_route53_record" "example" {
+#     zone_id = data.aws_route53_zone.example.zone_id
+#     name = data.aws_route53_zone.example.name
+#     type = "A"
 
-output "domain_name" {
-    value = aws_route53_record.example.name
-}
+#     alias {
+#         name = aws_lb.example.dns_name
+#         zone_id = aws_lb.example.zone_id
+#         evaluate_target_health = true
+#     }
+# }
+
+# output "domain_name" {
+#     value = aws_route53_record.example.name
+# }
+
+# resource "aws_acm_certificate" "example" {
+#     domain_name = aws_route53_record.example.name
+#     subject_alternative_names = []
+#     validation_method = "DNS"
+
+#     lifecycle {
+#         create_before_destroy = true
+#     }
+# }
+
+# resource "aws_route53_record" "example_certificate" {
+#     name = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
+#     type = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
+#     records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
+#     zone_id = data.aws_route53_zone.example.id
+#     ttl = 60
+# }
+
+# resource "aws_acm_certificate_validation" "example" {
+#     certificate_arn = aws_acm_certificate.example.arn
+#     validation_record_fqdns = [aws_route53_record.example_certificate.fqdn]
+# }
