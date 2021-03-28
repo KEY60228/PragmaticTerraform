b27d3720c8b5db6dfcb1695e79e36c7a1810da41
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-28 12:07:33 +0900

    Chapter.7.4 Serurity Group

--- src/main.tf
+++ src/main.tf
@@ -5,6 +5,14 @@ module "describe_regions_for_ec2" {
     identifier = "ec2.amazonaws.com"
 }
 
+module "example_sg" {
+    source = "./security_group"
+    name = "module-sg"
+    vpc_id = aws_vpc.example.id
+    port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_s3_bucket" "private" {
     bucket = "private-pragmatic-terraform-20210327"
 
