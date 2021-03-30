ddc299a38d50862c915141eecd189567bdea4ef1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-30 22:46:00 +0900

    Chapter.13.2 ElastiCache

--- src/main.tf
+++ src/main.tf
@@ -52,6 +52,14 @@ module "mysql_sg" {
     cidr_blocks = [aws_vpc.example.cidr_block]
 }
 
+module "redis_sg" {
+    source = "./security_group"
+    name = "redis-sg"
+    vpc_id = aws_vpc.example.id
+    port = 6379
+    cidr_blocks = [aws_vpc.example.cidr_block]
+}
+
 data "aws_iam_policy" "ecs_events_role_policy" {
     arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceEventsRole"
 }
@@ -592,4 +600,37 @@ resource "aws_db_instance" "example" {
     lifecycle {
         ignore_changes = [password]
     }
+}
+
+resource "aws_elasticache_parameter_group" "example" {
+    name = "example"
+    family = "redis5.0"
+
+    parameter {
+        name = "cluster-enabled"
+        value = "no"
+    }
+}
+
+resource "aws_elasticache_subnet_group" "example" {
+    name = "example"
+    subnet_ids = [aws_subnet.private_1a.id, aws_subnet.private_1c.id]
+}
+
+resource "aws_elasticache_replication_group" "example" {
+    replication_group_id = "example"
+    replication_group_description = "Cluster Disabled"
+    engine = "redis"
+    engine_version = "5.0.4"
+    number_cache_clusters = 3
+    node_type = "cache.t2.micro"
+    snapshot_window = "09:10-10:10"
+    snapshot_retention_limit = 7
+    maintenance_window = "mon:10:40-mon:11:40"
+    automatic_failover_enabled = true
+    port = 6379
+    apply_immediately = false
+    security_group_ids = [module.redis_sg.security_group_id]
+    parameter_group_name = aws_elasticache_parameter_group.example.name
+    subnet_group_name = aws_elasticache_subnet_group.example.name
 }
