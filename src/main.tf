281e512693a00ebeb69789222e15212f483667ee
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-03-30 22:21:53 +0900

    Chapter.12 SSM

--- src/main.tf
+++ src/main.tf
@@ -506,3 +506,20 @@ resource "aws_kms_key" "example" {
     deletion_window_in_days = 30
 }
 
+resource "aws_ssm_parameter" "db_username" {
+    name = "/db/username"
+    value = "root"
+    type = "String"
+    description = "データベースのユーザー名"
+}
+
+resource "aws_ssm_parameter" "db_raw_password" {
+    name = "/db/raw_password"
+    value = "VeryStrongPassword!"
+    type = "SecureString"
+    description = "データベースのパスワード"
+
+    lifecycle {
+        ignore_changes = [value]
+    }
+}
